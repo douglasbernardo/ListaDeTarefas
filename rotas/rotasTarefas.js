9cc8e16de5716943c5ac5bf015a5c7dcc7240b07
Douglas Bernardo de Melo <douglasbernardomelo@gmail.com>
2022-01-20 15:20:36 -0300

    adiciondo rota de exclusao

--- rotas/rotasTarefas.js
+++ rotas/rotasTarefas.js
@@ -6,6 +6,7 @@ const tarefaController = require("../controllers/tarefaController")
 router.get("",tarefaController.minhasTarefas)
 router.get("/adicionarTarefa",tarefaController.mostraFormulario)
 router.post("/adicionarTarefa",tarefaController.adicionarTarefa)
+router.post("/excluir/:id",tarefaController.excluirTarefa)
 
 
 module.exports = router
