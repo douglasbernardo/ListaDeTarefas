51cf5ff3bf7caf86f3afafaef0c8dac8bc71b51d
Douglas Bernardo de Melo <douglasbernardomelo@gmail.com>
2022-01-19 21:43:48 -0300

    adicionando novas rotas

--- rotas/rotasTarefas.js
+++ rotas/rotasTarefas.js
@@ -3,6 +3,7 @@ const router = require("express").Router()
 
 const tarefaController = require("../controllers/tarefaController")
 
+router.get("",tarefaController.minhasTarefas)
 router.get("/adicionarTarefa",tarefaController.mostraFormulario)
 router.post("/adicionarTarefa",tarefaController.adicionarTarefa)
 
