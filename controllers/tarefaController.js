51c54d5e531575ee3ec05eac42c2b118341ab47f
Douglas Bernardo de Melo <douglasbernardomelo@gmail.com>
2022-01-19 21:42:58 -0300

    nova função trazer todas as tarefas

--- controllers/tarefaController.js
+++ controllers/tarefaController.js
@@ -8,12 +8,35 @@ class tarefaController
         resp.render("tarefas/novaTarefa",{layout:false})
     }
 
-    static adicionarTarefa(req,resp){
+    static async adicionarTarefa(req,resp){
         
         const titulo = req.body.titulo
+        const descricao = req.body.descricao
 
-        console.log(titulo)
-       
+        const data = new Tarefa({titulo,descricao})
+        
+        if(!titulo && !descricao){
+            console.log("Preencha o titulo e a descrição")
+            return
+        }
+
+        if(!titulo){
+            console.log("Preencha o titulo corretamente")
+            return
+        }
+        if(!descricao){
+            console.log("Preencha a descrição corretamente")
+            return
+        }
+
+        await data.save()
+
+        resp.redirect('/tarefas')
+    }
+
+    static async minhasTarefas(req,resp){
+        const tarefas = await Tarefa.find().lean()
+        resp.render('tarefas/tarefas',{tarefas})
     }
 }
 
