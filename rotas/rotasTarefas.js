4b86ac91bca001c5d71e7f22d53f1976b1ca63ff
Douglas Bernardo de Melo <douglasbernardomelo@gmail.com>
2022-01-20 18:43:26 -0300

    rotas para mostrar formulario de edição e editar tarefa

--- rotas/rotasTarefas.js
+++ rotas/rotasTarefas.js
@@ -7,6 +7,8 @@ router.get("",tarefaController.minhasTarefas)
 router.get("/adicionarTarefa",tarefaController.mostraFormulario)
 router.post("/adicionarTarefa",tarefaController.adicionarTarefa)
 router.post("/excluir/:id",tarefaController.excluirTarefa)
+router.get("/editar/:id",tarefaController.formularioEdicao)
+router.post("/editar",tarefaController.editar)
 router.post("/done/:id",tarefaController.tarefaFeita)
 
 
