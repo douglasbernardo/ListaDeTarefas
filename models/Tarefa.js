c0197c666d5d883a92c85c3830804c7611e5a8e6
Douglas Bernardo de Melo <douglasbernardomelo@gmail.com>
2022-01-20 17:32:31 -0300

    Adicionando o campo status na Tarefa

--- models/Tarefa.js
+++ models/Tarefa.js
@@ -5,7 +5,8 @@ const Tarefa = mongoose.model(
     "Tarefa",
     new mongoose.Schema({
         titulo:{type:String,required:true},
-        descricao:{type:String,required:true}
+        descricao:{type:String,required:true},
+        status:{type:Boolean,required:true}
     })
 )
 
