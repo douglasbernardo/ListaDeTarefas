65f50094c7f5362edfda209909589abd709f8d11
Douglas Bernardo de Melo <douglasbernardomelo@gmail.com>
2022-01-22 17:14:24 -0300

    arquivo principal

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require('express')
 const app = express()
-const {flash} = require("express-flash-message")
+const cookieParser = require("cookie-parser")
 const session = require("express-session")
+const bodyParser = require("body-parser")
 const conn = require("./db/connection")
 
-const rotasTarefas = require("./rotas/rotasTarefas")
+app.use(bodyParser.json())
+app.use(cookieParser('secret'))
+app.use(session({cookie: {maxAge: null}}))
+
 
 //HANDLEBARS SETTINGS
 const handleBars = require("express-handlebars")
 
-//PARTIALES SETTINGS
 const hbs = handleBars.create({
-  partialsDir: ["views/partials/"],
+  partialsDir: ["views/partials/"], //partials path
 });
 
 app.engine("handlebars",hbs.engine)
@@ -20,19 +23,12 @@ app.set("view engine","handlebars")
 
 
 //FLASH MESSAGES SETTINGS
-app.use(
-  session({
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
-      // secure: true, // becareful set this option, check here: https://www.npmjs.com/package/express-session#cookiesecure. In local, if you set this to true, you won't receive flash as you are using `http` in local, but http is not secure
-    },
-  })
-)
+app.use((req, res, next)=>{
+  res.locals.message = req.session.message
+  delete req.session.message
+  next()
+})
 
-app.use(flash({ sessionKeyName: 'flashMessage' }));
 
 app.use(
   express.urlencoded({
@@ -44,7 +40,17 @@ app.use(express.json())
 
 app.use(express.static('public'))
 
+const rotasTarefas = require("./rotas/rotasTarefas")
 app.use('/tarefas',rotasTarefas)
 
 
+// app.get("/teste",(req,resp)=>{
+//   req.session.message = {
+//     type: 'danger',
+//     intro: 'Campos vazios! ',
+//     message: 'Preencha os campos corretamente.' 
+//   } 
+
+//   console.log(req.session.message.message)
+// })
 app.listen(2000,console.log("servidor rodando"))
