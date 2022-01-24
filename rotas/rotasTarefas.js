ec21052007713d2296ce8ee56dab1d094feb5791
douglasbernardodemelo <douglasbernardomelo@gmail.com>
2022-01-24 14:30:43 -0300

    tirando codigo não utilizavel

--- rotas/rotasTarefas.js
+++ rotas/rotasTarefas.js
@@ -3,12 +3,9 @@ const router = require("express").Router()
 
 const tarefaController = require("../controllers/tarefaController")
 
-
 router.get("",tarefaController.minhasTarefas)
 router.get("/adicionarTarefa",tarefaController.mostraFormulario)
-router.post("/adicionarTarefa",
-    tarefaController.adicionarTarefa
-)
+router.post("/adicionarTarefa",tarefaController.adicionarTarefa)
 router.post("/excluir/:id",tarefaController.excluirTarefa)
 router.get("/editar/:id",tarefaController.formularioEdicao)
 router.post("/editar",tarefaController.editar)
