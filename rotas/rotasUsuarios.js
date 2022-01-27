70bb2a1675a57cb73c67c2baa537daffbe0a5407
Douglas Bernardo de Melo <douglasbernardomelo@gmail.com>
2022-01-27 18:32:44 -0300

    todas as rotas de usuarios

--- rotas/rotasUsuarios.js
+++ rotas/rotasUsuarios.js
@@ -4,6 +4,7 @@ const router = require("express").Router()
 const usuarioController = require("../controllers/usuarioController")
 
 router.get("/cadastro",usuarioController.mostraFormulario)
+router.post("/cadastro",usuarioController.cadastro)
 
 
 module.exports = router
