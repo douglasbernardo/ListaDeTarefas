61cbd5540b15295cfd6366774d680aeef7cbd1ce
Douglas Bernardo de Melo <douglasbernardomelo@gmail.com>
2022-01-27 18:32:29 -0300

    criando função de cadastrar usuario

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,9 +1,18 @@
 
+const { validarUsuario } = require("../helpers/validacaoUsuario")
+const bcrypt = require("bcrypt")
 
 
 class usuarioController{
     static mostraFormulario(req,resp){
         resp.render("usuarios/cadastro",{layout:false})
+    }
+
+    static async cadastro(req,resp){
+        const {nome,email,senha,confirmarSenha} = req.body
+        await validarUsuario(req,resp,nome,email,senha,confirmarSenha)
+
+       //console.log(bcrypt.hash(senha))
     }
 }
 
