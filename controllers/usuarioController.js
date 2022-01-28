a9a57bda52174ae916c9f821c55898d0c496e279
Douglas Bernardo de Melo <douglasbernardomelo@gmail.com>
2022-01-27 22:14:50 -0300

    função para cadastrar usuarios

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,18 +1,27 @@
 
+const Usuario = require("../models/Usuario")
 const { validarUsuario } = require("../helpers/validacaoUsuario")
 const bcrypt = require("bcrypt")
 
 
-class usuarioController{
-    static mostraFormulario(req,resp){
+class usuarioController
+{
+
+    static mostraFormulario(req,resp)
+    {
         resp.render("usuarios/cadastro",{layout:false})
     }
 
-    static async cadastro(req,resp){
+    static async cadastro(req,resp)
+    {
         const {nome,email,senha,confirmarSenha} = req.body
-        await validarUsuario(req,resp,nome,email,senha,confirmarSenha)
+        if(!validarUsuario(req,resp,nome,email,senha,confirmarSenha)){
+            console.log("Alguma coisa de errado aconteceu!")
+        }
+        
+        console.log("Tudo ocorreu como deveria.")
 
-       //console.log(bcrypt.hash(senha))
+       console.log(bcrypt.hash(senha))
     }
 }
 
