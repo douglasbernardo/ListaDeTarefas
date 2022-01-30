032011effe7ae8f73e355b5bc2b76c857ec5f5ab
Douglas Bernardo de Melo <douglasbernardomelo@gmail.com>
2022-01-29 21:45:36 -0300

    rotas para os usuarios fazerem login ou cadastro

--- rotas/rotasUsuarios.js
+++ rotas/rotasUsuarios.js
@@ -3,8 +3,9 @@ const router = require("express").Router()
 
 const usuarioController = require("../controllers/usuarioController")
 
-router.get("/cadastro",usuarioController.mostraFormulario)
+router.get("/cadastro",usuarioController.formularioCadastro)
 router.post("/cadastro",usuarioController.cadastro)
-
+router.get("/login",usuarioController.formularioLogin)
+router.post("/login",usuarioController.login)
 
 module.exports = router
