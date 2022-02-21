17db7a6aaff539d4bd90c12b347484259ff114d6
douglasbernardodemelo <douglasbernardomelo@gmail.com>
2022-02-21 16:44:44 -0300

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const session = require("express-session")
-const {flash} = require("express-flash-message")
+const flash = require("express-flash")
 const conn = require("./db/connection")
 
 const FileStore = require("session-file-store")(session)
